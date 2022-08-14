4ed720f797199e70a296e309dfd8cc8c539822f4
HelithaSri <helitha.pravin@gmail.com>
2022-08-14 11:49:35 +0530

    User ui done

---
+++ src/controller/UserFormController.java
@@ -0,0 +1,28 @@
+package controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
+/**
+ * @author Helitha Sri
+ * @created 8/14/2022 - 11:18 AM
+ * @project Live Chat
+ */
+
+public class UserFormController {
+    public TextField txtMsgInput;
+    public TextArea txtMsgDisplay;
+
+    public void btnClose(ActionEvent actionEvent) {
+
+    }
+
+    public void btnMinimize(ActionEvent actionEvent) {
+        
+    }
+
+    public void btnSend(ActionEvent actionEvent) {
+        
+    }
+}
