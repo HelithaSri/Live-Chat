7abe75d2532f2526065b81663c411eb28d4d38e3
HelithaSri <helitha.pravin@gmail.com>
2022-08-14 14:33:07 +0530

    Update

--- src/controller/LoginFormController.java
+++ src/controller/LoginFormController.java
@@ -2,6 +2,16 @@ package controller;
 
 import com.jfoenix.controls.JFXTextField;
 import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
 
 /**
  * @author Helitha Sri
@@ -11,9 +21,40 @@ import javafx.event.ActionEvent;
 
 public class LoginFormController {
     public JFXTextField txtLoginName;
+    public AnchorPane logingPane;
 
-    public void btnLogin(ActionEvent actionEvent) {
+    private double xOffset = 0;
+    private double yOffset = 0;
 
+    public void btnLogin(ActionEvent actionEvent) throws IOException {
+        Stage stage = new Stage();
+        Parent root = FXMLLoader.load(getClass().getResource("../view/UserForm.fxml"));
+        Scene scene = new Scene(root);
+        stage.initStyle(StageStyle.TRANSPARENT); //Use For Boarder TRANSPARENT
+        root.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset=event.getSceneX();
+                yOffset=event.getSceneY();
+            }
+        });
+
+        root.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                stage.setX(event.getScreenX()-xOffset);
+                stage.setY(event.getScreenY()-yOffset);
+            }
+        });
+        stage.setMinWidth(440);
+        stage.setMinHeight(769);
+        stage.setMaxWidth(440);
+        stage.setMaxHeight(769);
+
+        stage.setScene(scene);
+        stage.centerOnScreen();
+        stage.show();
+        logingPane.getScene().getWindow().hide();
     }
 
     public void btnClose(ActionEvent actionEvent) {
