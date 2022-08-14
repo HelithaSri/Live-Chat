75c8a3c1ac84ccfa235c3e59bad159e321a7a8d6
HelithaSri <helitha.pravin@gmail.com>
2022-08-14 11:14:50 +0530

    Create Login Ui

---
+++ src/controller/LoginFormController.java
@@ -0,0 +1,26 @@
+package controller;
+
+import com.jfoenix.controls.JFXTextField;
+import javafx.event.ActionEvent;
+
+/**
+ * @author Helitha Sri
+ * @created 8/14/2022 - 10:36 AM
+ * @project Live Chat
+ */
+
+public class LoginFormController {
+    public JFXTextField txtLoginName;
+
+    public void btnLogin(ActionEvent actionEvent) {
+
+    }
+
+    public void btnClose(ActionEvent actionEvent) {
+
+    }
+
+    public void btnMinimize(ActionEvent actionEvent) {
+
+    }
+}
