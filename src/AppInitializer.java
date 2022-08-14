6a51b7ea7bead8b219a493735d76a78307ffe3ff
HelithaSri <helitha.pravin@gmail.com>
2022-08-14 12:33:55 +0530

    Made Undecorated window movable/draggable

--- src/AppInitializer.java
+++ src/AppInitializer.java
@@ -1,7 +1,9 @@
 import javafx.application.Application;
+import javafx.event.EventHandler;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
 
@@ -14,12 +16,32 @@ import java.net.URL;
  */
 
 public class AppInitializer extends Application {
+    private double xOffset = 0;
+    private double yOffset = 0;
+
     @Override
     public void start(Stage primaryStage) throws Exception {
         URL resource = getClass().getResource("view/LoginForm.fxml");
         Parent load = FXMLLoader.load(resource);
         Scene scene = new Scene(load);
-//        primaryStage.initStyle(StageStyle.TRANSPARENT); //Use For Boarder TRANSPARENT
+        primaryStage.initStyle(StageStyle.TRANSPARENT); //Use For Boarder TRANSPARENT
+
+        load.setOnMousePressed(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                xOffset=event.getSceneX();
+                yOffset=event.getSceneY();
+            }
+        });
+
+        load.setOnMouseDragged(new EventHandler<MouseEvent>() {
+            @Override
+            public void handle(MouseEvent event) {
+                primaryStage.setX(event.getScreenX()-xOffset);
+                primaryStage.setY(event.getScreenY()-yOffset);
+            }
+        });
+
         primaryStage.setMinWidth(458);
         primaryStage.setMinHeight(769);
         primaryStage.setMaxWidth(458);
